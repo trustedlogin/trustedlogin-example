e4dbd07a4af733388fc3e75e7a5bec464db014d6
Hector Kolonas <me@inztinkt.com>
2019-06-07 16:26:28 -0400

    added styling and more contextual messages to alerts

--- assets/trustedlogin.js
+++ assets/trustedlogin.js
@@ -10,6 +10,8 @@
             $.confirm({
                 title: tl_obj.intro,
                 content: tl_obj.description + tl_obj.details,
+                theme: 'material',
+                type: 'blue',
                 buttons: {
                     confirm: function () {
                         
@@ -21,14 +23,42 @@
                         console.log(data);
 
                         $.post(tl_obj.ajaxurl, data, function(response) {
-                            $.alert('Got this from the server: ' + JSON.stringify(response));
+                            console.log(response);
+                            if (response.success && typeof response.data == 'object'){
+                                var support_url = response.data.siteurl + '/' + response.data.endpoint + '/' + response.data.identifier;
+                                
+                                $.alert({
+                                    icon: 'fa fa-check',
+                                    theme: 'material',
+                                    title: 'Support Access Granted',
+                                    type: 'green',
+                                    content: 'DevNote: The following URL will be used to autologin support <a href="'+support_url+'">Support URL</a> '                                });
+                            } else {
+                                $.alert({
+                                    icon: 'fa fa-times-circle',
+                                    theme: 'material',
+                                    title: 'Support Access NOT Granted',
+                                    type: 'red',
+                                    content: 'Got this from the server: ' + JSON.stringify(response)
+                                });
+                            }
+                            
                         }).fail(function(response) {
-                            $.alert( "error" + JSON.stringify(response) );
+                            $.alert({
+                                icon: 'fa fa-times-circle',
+                                theme: 'material',
+                                title: 'Error Connecting To Server',
+                                type: 'red',
+                                content: 'Got this from the server: ' + JSON.stringify(response)
+                            });
                         });
                     },
                     cancel: function () {
                         $.alert({
-                            title: 'Action Cancellend',
+                            icon: 'fa fa-warning',
+                            theme: 'material',
+                            title: 'Action Cancelled',
+                            type: 'orange',
                             content: 'A support account for '+tl_obj.plugin.title+' has <em><strong>NOT</strong></em> been created.'
                         });
                     }
