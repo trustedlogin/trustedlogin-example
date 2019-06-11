eb533d315f70fcb5308d7732323b1a13cc52af5d
Hector Kolonas <me@inztinkt.com>
2019-06-11 15:01:36 -0400

    patch: make sure to redirect support agent after revoking TL access

--- includes/class-trustedlogin.php
+++ includes/class-trustedlogin.php
@@ -813,7 +813,7 @@ class TrustedLogin
         }
 
         if ($success) {
-            if (!is_user_logged_in() && !current_user_can('administrator')) {
+            if (!is_user_logged_in() || !current_user_can('administrator')) {
                 wp_redirect(home_url());
                 die;
             } else {
