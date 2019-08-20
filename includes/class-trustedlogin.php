00a9ec736d6e78a91d4d38b6058ae72f537f3a13
Zack Katz <zack@katz.co>
2019-08-19 18:25:48 -0600

    Add extra log

--- includes/class-trustedlogin.php
+++ includes/class-trustedlogin.php
@@ -1293,6 +1293,7 @@ class TrustedLogin {
 			add_action( 'admin_notices', array( $this, 'admin_notice_revoked' ) );
 		}
 
+		$this->log( 'User #' . $support_user->ID .' was not removed', __METHOD__, 'error' );
 	}
 
 	/**
